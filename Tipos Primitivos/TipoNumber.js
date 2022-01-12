2017788cc5e0fc91a216ab964374b84ef2a76c70
Filipe Trigueiro <filipe1514@gmail.com>
2022-01-12 19:08:37 -0300

    exemplos da aula 1

---
+++ Tipos Primitivos/TipoNumber.js
@@ -0,0 +1,34 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+console.log(novaOperacao);
+
+
+// NaN ->  Not A Number (Não é um número)
+
+const  alura = "Alura";
+console.log(alura*primeiroNumero);
+
+
+/*
+var a = 10;
+var b = 0;
+console.log(a/b);
+*/
+
---
+++ Tipos Primitivos/type-string.js
@@ -0,0 +1,40 @@
+/*
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+//const citacao = 'O Leo disse "oi!"';
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+//console.log(citacao);
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+
+
+//template string ou template literal
+*/
+/*
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+*/
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo);
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
